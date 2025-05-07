9bcd4f0d67c8745f69927cdb2d85b9dc517d684d
Marilia Gabriele <mariliagabriele081@gmail.com>
2025-05-07 16:53:18 -0300

    init auth

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,18 +1,25 @@
 import express from 'express'
-import {createUser, getAllUsers, deleteUser, updateUser, getUserId} from "../controllers/userController.js"
+import {createUser,
+     getAllUsers,
+     deleteUser,
+     updateUser,
+     getUserId,
+     registerUser
+    } from "../controllers/userController.js"
 import { validate } from '../middleware/validate.js'
 import {createUserSchema, updateUserSchema} from '../schemas/userSchemas.js'
-
 
 const router = express.Router()
 
 router.get ('/', getAllUsers)
-router.post ('/', validate(createUserSchema), createUser)
+router.post ('/', validate(createUserSchema),  createUser)
 
 router.put('/:id', validate(updateUserSchema), updateUser)
 
 router.delete('/:id', deleteUser)
 
 router.get('/:id', getUserId)
+
+router.post('/register', registerUser)
 
 export default router
